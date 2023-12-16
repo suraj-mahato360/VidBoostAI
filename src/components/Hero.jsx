ba5071f3db80fb7b8a680610b863ef637646db8e
sushan4 <uchilsushan36@gmail.com>
2023-12-16 17:08:06 +0530

    Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,19 +1,21 @@
-const Navbar = () => {
+import React from "react";
+
+const Hero = () => {
   return (
     <section>
-      <div class="bg-black text-white py-20">
-        <div class="container mx-auto flex flex-col md:flex-row items-center my-12 md:my-24">
-          <div class="flex flex-col w-full lg:w-1/3 justify-center items-start p-8">
-            <h1 class="text-3xl md:text-5xl p-2 text-yellow-300 tracking-loose">
+      <div className="bg-black text-white py-20">
+        <div className="container mx-auto flex flex-col md:flex-row items-center my-12 md:my-24">
+          <div className="flex flex-col w-full lg:w-1/3 justify-center items-start p-8">
+            <h1 className="text-3xl md:text-5xl p-2 text-yellow-300 tracking-loose">
               VidBoost AI
             </h1>
-            <h2 class="text-3xl md:text-5xl leading-relaxed md:leading-snug mb-2">
+            <h2 className="text-3xl md:text-5xl leading-relaxed md:leading-snug mb-2">
               One Stop Tool for all your Youtube Needs!
             </h2>
-            <p class="text-sm md:text-base text-gray-50 mb-4">Be</p>
+            <p className="text-sm md:text-base text-gray-50 mb-4">Be</p>
             <a
               href="#"
-              class="bg-transparent hover:bg-yellow-300 text-yellow-300 hover:text-black rounded shadow hover:shadow-lg py-2 px-4 border border-yellow-300 hover:border-transparent"
+              className="bg-transparent hover:bg-yellow-300 text-yellow-300 hover:text-black rounded shadow hover:shadow-lg py-2 px-4 border border-yellow-300 hover:border-transparent"
             >
               Lorem ipsum dolor sit amet, consectetur adipisicing elit. Possimus
               voluptas consequatur maxime, doloremque beatae repellat voluptates
@@ -21,24 +23,27 @@ const Navbar = () => {
               Officia tenetur sit sunt animi?
             </a>
           </div>
-          <div class="p-8 mt-12 mb-6 md:mb-0 md:mt-0 ml-0 md:ml-12 lg:w-2/3  justify-center">
-            <div class="h-48 flex flex-wrap content-center">
+          <div className="p-8 mt-12 mb-6 md:mb-0 md:mt-0 ml-0 md:ml-12 lg:w-2/3 justify-center">
+            <div className="h-48 flex flex-wrap content-center">
               <div>
                 <img
-                  class="inline-block mt-28 hidden xl:block"
+                  className="inline-block mt-28 hidden xl:block"
                   src="https://user-images.githubusercontent.com/54521023/116969935-c13d5b00-acd4-11eb-82b1-5ad2ff10fb76.png"
+                  alt="Image 1"
                 />
               </div>
               <div>
                 <img
-                  class="inline-block mt-24 md:mt-0 p-8 md:p-0"
+                  className="inline-block mt-24 md:mt-0 p-8 md:p-0"
                   src="https://user-images.githubusercontent.com/54521023/116969931-bedb0100-acd4-11eb-99a9-ff5e0ee9f31f.png"
+                  alt="Image 2"
                 />
               </div>
               <div>
                 <img
-                  class="inline-block mt-28 hidden lg:block"
+                  className="inline-block mt-28 hidden lg:block"
                   src="https://user-images.githubusercontent.com/54521023/116969939-c1d5f180-acd4-11eb-8ad4-9ab9143bdb50.png"
+                  alt="Image 3"
                 />
               </div>
             </div>
@@ -48,3 +53,5 @@ const Navbar = () => {
     </section>
   );
 };
+
+export default Hero;
