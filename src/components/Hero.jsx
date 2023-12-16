26844c5357de096ea2fb67c4828b27ab66d5d70c
Aditya <adityargamer6@gmail.com>
2023-12-16 18:09:41 +0530

    Hero

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,16 +3,15 @@ import React from "react";
 const Hero = () => {
   return (
     <section>
-      <div className="bg-black text-white py-20">
+      <div className="bg-black text-white py-4">
         <div className="container mx-auto flex flex-col md:flex-row items-center my-12 md:my-24">
           <div className="flex flex-col w-full lg:w-1/3 justify-center items-start p-8">
-            <h1 className="text-3xl md:text-5xl p-2 text-yellow-300 tracking-loose">
+            <h1 className="text-3xl md:text-5xl  text-yellow-300 tracking-loose">
               VidBoost AI
             </h1>
-            <h2 className="text-3xl md:text-5xl leading-relaxed md:leading-snug mb-2">
+            <h2 className="text-3xl md:text-4xl leading-relaxed md:leading-snug mb-10">
               One Stop Tool for all your Youtube Needs!
             </h2>
-            <p className="text-sm md:text-base text-gray-50 mb-4">Be</p>
             <a
               href="#"
               className="bg-transparent hover:bg-yellow-300 text-yellow-300 hover:text-black rounded shadow hover:shadow-lg py-2 px-4 border border-yellow-300 hover:border-transparent"
